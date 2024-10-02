4ef6e3e28a4ede8f39a677d951f4b13a57005995
AZR437 <106365725+AZR437@users.noreply.github.com>
2024-10-02 18:54:29 +0800

    Update AppWindow.cpp

--- DirectX Game/AppWindow.cpp
+++ DirectX Game/AppWindow.cpp
@@ -21,53 +21,7 @@ void AppWindow::OnCreate()
 	UINT height = rc.bottom - rc.top;
 	this->swapChain->Init(this->hwnd, width, height);
 
-	//vertex list[] =
-	//{
-	//	//X - Y - Z
-	//	{-0.5f,-0.5f,0.0f,    -0.32f,-0.11f,0.0f,   1,0,0,  0,0,1 }, // POS1
-	//	{-0.5f,0.5f,0.0f,     -0.11f,0.78f,0.0f,    1,0,0,  0,0,1 }, // POS2
-	//	{ 0.5f,-0.5f,0.0f,     0.75f,-0.73f,0.0f,   1,0,0,  0,0,1 },// POS2
-	//	{ 0.5f,0.5f,0.0f,      0.88f,0.77f,0.0f,    1,0,0,  0,0,1 }
-	//};
-	//this->vertexBuffer = GraphicsEngine::GetInstance()->CreateVertexBuffer();
-	//UINT listSize = ARRAYSIZE(list);
-	//
-
-
-	//
-
-	//void* shaderByteCode = nullptr;
-	//size_t shaderSize = 0;
-
-	//GraphicsEngine::GetInstance()->CompileVertexShaders(L"VertexShader.hlsl", "vsmain", &shaderByteCode, &shaderSize);
-
-	//this->vertexShader = GraphicsEngine::GetInstance()->CreateVertexShaders(shaderByteCode, shaderSize);
-
-	//this->vertexBuffer->Load(list, sizeof(vertex), listSize, shaderByteCode, shaderSize);
-
-	//GraphicsEngine::GetInstance()->ReleaseCompiledShader();
-
-	//GraphicsEngine::GetInstance()->CompilePixelShader(L"PixelShader.hlsl", "psmain", &shaderByteCode, &shaderSize);
-
-	//this->pixelShader = GraphicsEngine::GetInstance()->CreatePixelShader(shaderByteCode, shaderSize);
-
-	//GraphicsEngine::GetInstance()->ReleaseCompiledShader();
-
-	/*constant cc;
-	cc.angle = 0;
-
-	this->constantBuffer = GraphicsEngine::GetInstance()->CreateConstantBuffer();
-	this->constantBuffer->Load(&cc, sizeof(constant));*/
-
-
-
-
 	
-	
-	//this->quadList.push_back(Quad(0, 0, 0.45f));
-	//this->quadList.push_back(Quad(1.5f, 0, 0.45f));
-	//this->quadList.push_back(Quad(-1.5f, 0, 0.45f));
-	//this->triList.push_back(Triangles(-1.5, -1.5, 0.45));
 
 
 
@@ -101,28 +55,7 @@ void AppWindow::OnUpdate()
 	UINT height = rc.bottom - rc.top;
 	GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->SetViewPortSize(width, height);
 
-	//unsigned long newTime = 0;
-	//if (this->oldTime)
-	//	newTime = ::GetTickCount() - this->oldTime;
-	//this->deltaTime = newTime / 1000.0f;
-	//this->oldTime = ::GetTickCount();
-
-	//this->angle += 1.57f * this->deltaTime;
-	//constant cc;
-	//cc.angle = this->angle;
-
-	//this->constantBuffer->Update(GraphicsEngine::GetInstance()->GetImmediateDeviceContext(), &cc);
-
-	//GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->SetConstantBuffer(this->vertexShader, this->constantBuffer);
-	//GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->SetConstantBuffer(this->pixelShader, this->constantBuffer);
-
-	////set default shader in graphics pipeline
-	//GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->SetVertexShader(this->vertexShader);
-	//GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->SetPixelShader(this->pixelShader);
-	////Set vertices
-	//GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->SetVertexBuffer(this->vertexBuffer);
-	//// Draw
-	//GraphicsEngine::GetInstance()->GetImmediateDeviceContext()->DrawTriangleStrip(this->vertexBuffer->GetVertexSizelist(), 0);
+	
 
 	
 
@@ -133,13 +66,6 @@ void AppWindow::OnUpdate()
 void AppWindow::OnDestroy()
 {
 	this->swapChain->Release();
-	//this->vertexBuffer->Release();
-	//this->vertexShader->Release();
-	//this->pixelShader->Release();
-	//this->quadList[0].Release();
-	//this->quadList[1].Release();
-	//this->quadList[2].Release();
-	//this->triList[0].Release();
 	this->primitiveLoader->ReleasePrimitives();
 	Window::OnDestroy();
 }
